6a235009cd7ca8490beec9d969f8f02e0d4ceac7
nur <fatmanur_agacbacak@hotmail.com>
2024-11-18 17:55:52 +0300

    DataAccessLayer

--- CSharpEgitimKampi301/CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
+++ CSharpEgitimKampi301/CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,22 +9,11 @@ namespace CSharpEgitimKampi301.EntityLayer.Concrete
 {
     public class Category
     {
-
-        /* field variable property arasında farklar
-         * eğer bir değişken direkt sınıfın içine tanımlanırsa field olarak karşılar
-         * eğer o değişkenin yapısı get set değerlerini alırsa property olur
-         * metot içine tanımlanırsa değişken(variable) olur
-         * 
-         * int x; --> field 
-         * int x {get; set;} --> property
-         * public sayi()
-         * {
-         *      int x; --> variable
-         * }
-        */
-
+    
         public int CategoryId { get; set; }
         public string CategoryName { get; set; }
         public bool CategoryStatus { get; set; }
+
+        public List<Product> Products { get; set; }
     }
 }
